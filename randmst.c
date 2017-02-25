769eed71c95870337a7396c49ea4e12b315979b5
HSADev2 <hsadev2@HSADev2s-iMac.local>
2017-02-25 16:37:34 -0500

    attempting naive prims

--- randmst.c
+++ randmst.c
@@ -200,14 +200,14 @@ void destroyGraph(graph_p g)
 // set_p prim(graph_p g);
 void prim(graph_p g)
 {
-    // initialize dist, prev arrays of size V
+    // initialize prev, prev arrays of size V
 
-    //set_p dist = (set_p) malloc(g->V * sizeof(set_t));
-    set_p dist[g->V];// = (set_p) malloc(sizeof(set_t) * );
+    //set_p prev = (set_p) malloc(g->V * sizeof(set_t));
+    set_p prev[g->V];// = (set_p) malloc(sizeof(set_t) * );
     int i, d, index;
-    d = sizeof(dist);
+    d = sizeof(prev);
     index = 0;
-    for(i = 0; i < d; i += sizeof(dist[i]))
+    for(i = 0; i < d; i += sizeof(prev[i]))
     {
         // printf("index equals: %i\n", index);
         // printf("i equals:  %i\n", i);
@@ -215,12 +215,12 @@ void prim(graph_p g)
         // printf("WEIGHT: %f\n\n", g->alistArr[index].head->wt);
 
 
-        //dist[i]->setV = createVertex(g->alistArr[index].head=>vertex);
-        //dist[i]->setV->vertex = g->alistArr[index].head=>vertex;
+        //prev[i]->setV = createVertex(g->alistArr[index].head=>vertex);
+        //prev[i]->setV->vertex = g->alistArr[index].head=>vertex;
 
-        //dist[i]->setV->wt = g->alistArr[index].head->wt;
+        //prev[i]->setV->wt = g->alistArr[index].head->wt;
 
-        //printf("dist[i] :  %f\n", dist[i]->setV->wt);
+        //printf("prev[i] :  %f\n", prev[i]->setV->wt);
         index++;
     }
     return;
