182dfa226dc285874bce729426eae755dce8e5fb
HSADev2 <hsadev2@HSADev2s-iMac.local>
2017-02-27 14:42:31 -0500

    cleaned code and about to fix dimension 0

--- randmst.c
+++ randmst.c
@@ -8,11 +8,10 @@
 
 #define MAX_WT 1000.0
 
-/* Adjacency list node*/
 typedef struct node
 {
-    int vertex;                /*Index to adjacency list array*/
-    struct node *next; /*Pointer to the next node*/
+    int vertex;
+    struct node *next;
     double wt;
     double x;
     double y;
@@ -21,6 +20,7 @@ typedef struct node
 } node_t, *node_p;
 
 
+// stores wt or coordinates for node and assigns vertex ID
 node_p createNode(int v, int dimension)
 {
     node_p newNode = (node_p)malloc(sizeof(node_t));
@@ -51,6 +51,7 @@ node_p createNode(int v, int dimension)
     return newNode;
 }
 
+
 // instantiate vertices
 node_p* createArrOfNodes(int n, int dimension)
 {
@@ -59,28 +60,29 @@ node_p* createArrOfNodes(int n, int dimension)
 
     int i, idx;
     node_p newNode;
+
     for(i = 0; i < n; i++)
     {
         newNode = createNode(i, dimension);
-        //printf("%f\n", newNode->x);
-        vertices[i] = newNode;
-        // idx += sizeof(node_p);
-        // vertices[i] = newNode;
-        //printf("VERTEX ID : %i\n", vertices[i]->vertex);
 
+        vertices[i] = newNode;
     }
 
     return vertices;
 }
 
 
-// only call this on dimension 2, 3, 4
+// only call on dimension 2, 3, 4
 double calcEuclidian(node_p* vertices, int idx_1, int idx_2, int dimension)
 {
     node_p node_1 = vertices[idx_1];
     node_p node_2 = vertices[idx_2];
     double sumDiffSquare;
     sumDiffSquare = 0.0;
+    if (dimension == 0)
+    {
+        return node_2->wt;
+    }
     if (dimension >= 2)
     {
         sumDiffSquare = pow(node_1->x - node_2->x, 2) + pow(node_1->y - node_2->y, 2);
@@ -96,86 +98,53 @@ double calcEuclidian(node_p* vertices, int idx_1, int idx_2, int dimension)
     return sqrt(sumDiffSquare);
 }
 
-// double minKey(double key[], bool mstSet[], int V)
-// {
-//    // Initialize min value
-//    double min = INT_MAX;
-//    double min_index;
-//
-//    for (int v = 0; v < V; v++)
-//    {
-//        if (mstSet[v] == false && key[v] < min)
-//        {
-//            min = key[v];
-//            min_index = v;
-//        }
-//    }
-//    return min_index;
-// }
-
-double primMST(node_p * vertices, int V, int dimension)
+double mstWt(node_p * vertices, int V, int dimension)
 {
-     double parent[V]; // Array to store constructed MST
-     double key[V];   // Key values used to pick minimum weight edge in cut
-     bool mstSet[V];  // To represent set of vertices not yet included in MST
-     double tmp_wt;
-     // Initialize all keys as INFINITE
-     int i;
-     for (i = 0; i < V; i++)
-     {
-         key[i] = INT_MAX;
-         mstSet[i] = false;
-     }
+    double dist_keys[V];
+    double dist_wts[V];
+    bool in_mst[V];
+    int i, u, v, min_key;
+    double tmp_wt, tot_wt, min_wt;
 
-     key[0] = 0;     // Make key 0 so that this vertex is picked as first vertex
-     parent[0] = -1; // First node is always root of MST
+    for(i = 0; i < V; i++)
+    {
+        dist_wts[i] = INT_MAX;
+        in_mst[i] = false;
+    }
 
-     //double min;
-     //double min_index;
-     int u;
-     u = 0;
-     for (int count = 0; count < V-1; count++)
-     {
-        //int u = minKey(key, mstSet, V);
+    dist_wts[0] = 0;
+    dist_keys[0] = 0;
 
-        //min = INT_MAX;
-
-        //Initialize min value
-       double min = INT_MAX;
-       double min_index;
-
-       for (int v = 0; v < V; v++)
-       {
-           if (mstSet[v] == false && key[v] < min)
-           {
-               min = key[v];
-               u = v;
-           }
-       }
-
-        mstSet[u] = true;
-
-
-        for (int v = 0; v < V; v++)
+    min_key = 0;
+    for(i = 0; i < V - 1; i++)
+    {
+        min_wt = INT_MAX;
+        for(u = 0; u < V; u++)
         {
-            tmp_wt = calcEuclidian(vertices, u, v, dimension);
-            //printf("tmp_wt: %f\n", tmp_wt);
-            if (mstSet[v] == false && tmp_wt <  key[v]) // graph[u][v] &&???
+            if (in_mst[u] == false && dist_wts[u] < min_wt)
             {
-                parent[v]  = u;
-                key[v] = tmp_wt;
+                min_wt = dist_wts[u];
+                min_key = u;
             }
         }
+        in_mst[min_key] = true;
+        for(v = 0; v < V; v++)
+        {
+            tmp_wt = calcEuclidian(vertices, min_key, v, dimension);
+            if(in_mst[v] == false && tmp_wt < dist_wts[v])
+            {
+                dist_keys[v] = min_key;
+                dist_wts[v] = tmp_wt;
+            }
+        }
+    }
+    tot_wt = 0.0;
+    for(i = 0; i < V; i++)
+    {
+        tot_wt += dist_wts[i];
+    }
+    return tot_wt;
 
-     }
-     double totWt;
-     totWt = 0.0;
-     for(i = 0; i < V; i++)
-     {
-        //  printf("DISTANCE: %f\n", key[i]);
-         totWt += key[i];
-     }
-     return totWt;
 }
 
 int main(int argc, char* argv[])
@@ -191,22 +160,27 @@ int main(int argc, char* argv[])
     numpts = atoi(argv[2]);
     numtrials = atoi(argv[3]);
 	dimension = atoi(argv[4]);
+
     if (dimension != 0 && dimension != 2 && dimension != 3 && dimension != 4)
     {
         printf("Please enter valid dimensions (ie. 0, 2, 3, 4)\n");
         return 1;
     }
+    if (numpts > 131073)
+    {
+        printf("values of this magnitude have not been tested. proceed at own peril\n");
+        return 1;
+    }
 
-    int i, ind;
+    int i;
     double totWt;
     totWt = 0.0;
-    //node_p vertices[numpts];
+
     for(i = 0; i < numtrials; i++)
     {
         node_p * vertices = createArrOfNodes(numpts, dimension);
 
-        totWt += primMST(vertices, numpts, dimension);
-        //destroyGraph(g);
+        totWt += mstWt(vertices, numpts, dimension);
     }
     printf("AVG WT: %f\n", totWt / (double) numtrials);
 
